5d58cae9550cb78943550d12a201d5e206d795b0
Davin An <blueapple1030@gmail.com>
2026-08-29 15:48:23 +0900

    Solved : Basic Problem 09_number_theory.py

--- week2/1. basic/09_number_theory.py
+++ week2/1. basic/09_number_theory.py
@@ -24,6 +24,8 @@
 - LCM 공식: lcm(a, b) = (a × b) / gcd(a, b)
 """
 
+import math
+
 def gcd(a, b):
     """
     유클리드 호제법을 사용한 최대공약수 계산
@@ -37,7 +39,16 @@ def gcd(a, b):
     # TODO: 유클리드 호제법 구현
     # base case: b가 0이면 a 반환
     # recursive를 이용 
+    if b == 0:
+        return a
+    if a > b:
+        return gcd(b, a % b)
+    if a < b:
+        return gcd(a, b % a)
     pass
+
+
+
 
 def gcd_iterative(a, b):
     """
@@ -51,7 +62,20 @@ def gcd_iterative(a, b):
     """
     # TODO: 반복문으로 구현
     # b가 0이 될 때까지 반복
+    if a > b:
+        nums = [a, b]
+    if a < b:
+        nums = [b, a]
+    
+    while (nums[1] != 0):
+        nums[0], nums[1] = nums[1], nums[0]
+        nums[1] = (nums[1] % nums[0])
+    return nums[0]
     pass
+
+
+
+
 
 def lcm(a, b):
     """
@@ -64,7 +88,12 @@ def lcm(a, b):
         최소공배수
     """
     # TODO: LCM 계산
+    return int((a*b)/gcd(a, b))
     pass
+
+
+
+
 
 def extended_gcd(a, b):
     """
@@ -81,7 +110,17 @@ def extended_gcd(a, b):
     # base case: b가 0이면 (a, 1, 0) 반환    
     # recursive case
     # 역추적하며 x, y 계산
+    if b == 0:
+        return (a, 1, 0)
+    gcd_val = gcd(a, b)
+    x0 = extended_gcd(b, a%b)[2]
+    y0 = extended_gcd(b, a%b)[1] - (a // b) * extended_gcd(b, a%b)[2]
+    
+    return (gcd_val, x0, y0)
     pass
+
+
+
 
 def is_prime(n):
     """
@@ -97,7 +136,17 @@ def is_prime(n):
     # n이 2보다 작으면 False
     # 2부터 sqrt(n)까지 나누어 떨어지는지 확인    
     # 3부터 sqrt(n)까지 홀수만 확인
+    if n < 2:
+        return False
+    for i in range (2, int(math.sqrt(n))+1):
+        if n%i == 0:
+            return False
+    return True
     pass 
+
+
+
+
 
 # 테스트 케이스
 if __name__ == "__main__":
