78a8bc0951123780364f234b1f2645b9e2dff406
Davin An <blueapple1030@gmail.com>
2026-08-29 21:22:31 +0900

    Solved : Basic Problem 13_queue.py

--- week2/1. basic/13_queue.py
+++ week2/1. basic/13_queue.py
@@ -38,15 +38,22 @@ def process_print_queue(jobs):
     """
     # TODO: deque로 큐 생성
     queue = deque(jobs)
-    
     processed = []
     
     # TODO: 큐가 비어있지 않은 동안 반복
     ## 큐에서 작업 꺼내기
     ## 작업 처리 (출력 및 리스트에 추가)
+    while (len(queue) != 0):
+        print("처리: " + queue[0])
+        processed.append(queue[0])
+        queue.popleft()
     pass
     
     return processed
+
+
+
+
 
 # 테스트 케이스
 if __name__ == "__main__":
